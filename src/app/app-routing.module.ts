9753ebf9d651fc10cba257dd0a4c8e7e545f688d
muneer <muneertech2@gmail.com>
2021-02-18 15:40:53 -0700

    lab1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditTodoComponent } from './todos/add-edit-todo/add-edit-todo.component';
+import {TodosListComponent} from './todos/todos-list/todos-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'list',component: TodosListComponent},
+{path:'add-edit',component: AddEditTodoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
